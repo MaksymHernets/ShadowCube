8a45915b8a5fd02616412b1d55618b32d5498986
Maksym Hernets <gernets.maksim@gmail.com>
2022-08-06 19:05:30 +0300

    Added BindRuntimePlatformConfig and GenericRuntimePlatform

---
+++ Assets/Scripts/Generic/GenericRuntimePlatform.cs
@@ -0,0 +1,15 @@
+namespace Generic
+{
+	public enum GenericRuntimePlatform
+    {
+        Editor = 0,
+        PC = 1,
+        Phone = 2,
+        Console = 3,
+        Web = 4,
+        PortConsole = 5,
+        VR = 6,
+        TV = 7,
+        Unknown = 999,
+    }
+}
